427ea527ed003dac61321962e26110fc601ee367
Abhinav Sinha <abhinavsinha135@gmail.com>
2023-05-24 11:23:01 +0530

    first commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,45 @@
+import Nav from "./Nav";
+import Banner from "./Banner";
+import Row from "./Row";
+import requests from "./Requests";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        fetchURL={requests.fetchTrending}
+      />
+      <Row
+        title="Action Movies"
+        fetchURL={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        fetchURL={requests.fetchComedyMovies}
+      />
+      <Row
+        title="Horror Movies"
+        fetchURL={requests.fetchHorrorMovies}
+      />
+      <Row
+        title="Romance Movies"
+        fetchURL={requests.fetchRomanceMovies}
+      />
+      <Row
+        title="Documentaries"
+        fetchURL={requests.fetchDocumentaries}
+      />
+    </div>
+  );
+};
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZH8EKLsLXAQg-OqkVSQOmmuxBTLsi_7Y",
+  authDomain: "netflix-clone-475c2.firebaseapp.com",
+  projectId: "netflix-clone-475c2",
+  storageBucket: "netflix-clone-475c2.appspot.com",
+  messagingSenderId: "35886248469",
+  appId: "1:35886248469:web:9a4a853023cfa02781d41c",
+};
